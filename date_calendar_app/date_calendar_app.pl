9a8e0c0f93aad545b49dd2e46998dcae96a1e51f
luckywyy <862024320@qq.com>
2023-01-09 13:43:18 +0800

    Update date_calendar_app.pl

--- date_calendar_app/date_calendar_app.pl
+++ date_calendar_app/date_calendar_app.pl
@@ -6,6 +6,63 @@ use Encode qw(decode_utf8);
 use Time::Piece;
 use Time::Seconds;
 
+
+
+
+my $note_dir = './day_note';
+
+sub get_datetime {
+  my $datetime = `date +%Y-%m-%d,%H:%M:%S`;
+  $datetime =~ s/,/ /g;
+  chomp $datetime;
+  return $datetime;
+};
+
+sub calc_today_relative {
+  my $date = shift;
+  my ($y, $m, $d) = ($1, $2, $3) if $date =~ m/(.*)-(.*)-(.*)/;
+
+  my @month_days = ();
+
+  my $prev_date = $date;
+  while($d != 1) {
+    $prev_date = `date -d "$prev_date -1 day" +%y-%m-%d`;
+    chomp $prev_date;
+    $d = $1 if $prev_date =~ m/.*-.*-(.*)/;
+  }
+  # push(@month_days, '01');
+  $prev_date = `date -d "$prev_date -1 day" +%y-%m-%d`;
+  while(1) {
+    $prev_date = `date -d "$prev_date +1 day" +%y-%m-%d`;
+    chomp $prev_date;
+    $d = $1 if $prev_date =~ m/.*-.*-(.*)/;
+    my $week_day = `date -d "$prev_date" +%W`;
+    chomp $week_day;
+
+    my $tmp_y = `date -d "$prev_date" +%Y`;
+    chomp $tmp_y;
+    my $tmp_m = `date -d "$prev_date" +%m`;
+    chomp $tmp_m;
+    my $tmp_d = `date -d "$prev_date" +%d`;
+    chomp $tmp_d;
+    my $file_path = "$note_dir/$tmp_y-$tmp_m-$tmp_d.txt";
+
+    my $content_flag = 0;
+    $content_flag = 1 if -e $file_path;
+
+    my $flag_m = $1 if $prev_date =~ m/.*-(.*)-.*/;
+    if ($flag_m != $m) {
+      last;
+    }
+    push(@month_days, [$d, $week_day, $content_flag]);
+  }
+  # @month_days = sort @month_days;
+
+  return {
+    month_days => \@month_days,
+  };
+};
+
 # input format: 2022-12-12
 sub calc_tgdz {
   my $date = shift;
@@ -107,6 +164,8 @@ post '/api/calc_date' => sub ($c) {
   $sec_date_info_hash->{sec_m} = $sec_m;
   $sec_date_info_hash->{sec_d} = $sec_d;
 
+  my $today_relative = calc_today_relative($fir_date);
+
   $c->flash(fir_date => $fir_date);
   $c->flash(fir_date_info_hash => $fir_date_info_hash);
   $c->flash(sec_date => $sec_date);
@@ -114,8 +173,54 @@ post '/api/calc_date' => sub ($c) {
   $c->flash(single_date_flag => $single_date_flag);
   $c->flash(msg => $msg);
   $c->flash(main_info => $main_info);
+  $c->flash(today_relative => $today_relative);
 
   $c->redirect_to('date');
+};
+
+post '/query/:y/:m/:d' => sub ($c) {
+  my $y = $c->stash('y');
+  my $m = $c->stash('m');
+  my $d = $c->stash('d');
+
+  `mkdir $note_dir` unless -d $note_dir;
+
+  my $file_path = "$note_dir/$y-$m-$d.txt";
+  `touch $file_path` unless -e $file_path;
+
+  my $content = "no note";
+  $content = `cat $file_path`;
+  chomp $content;
+
+  my $content_ = '';
+  my $dt_ = '';
+  if ($content eq '') {
+    $content = 'no note';
+  } else {
+    my @content = split('\[br:/br\]', $content);
+    $content_ = decode_utf8($1) if $content[-1] =~ m/\[note:(.*)\/note\]/;
+    $dt_ = $1 if $content[-1] =~ m/\[datetime:(.*)\/datetime\]/;
+  }
+
+  $c->render(json => {content => $content_, dt => $dt_});
+};
+
+post '/add/:y/:m/:d' => sub ($c) {
+  my $y = $c->stash('y');
+  my $m = $c->stash('m');
+  my $d = $c->stash('d');
+
+  my $note = decode_utf8($c->req->body_params->{'string'});
+
+  `mkdir $note_dir` unless -d $note_dir;
+
+  my $file_path = "$note_dir/$y-$m-$d.txt";
+  `touch $file_path` unless -e $file_path;
+
+  my $datetime = get_datetime();
+  `echo '[note:$note/note]' [datetime:$datetime/datetime] [br:/br] >> $file_path`; #使用 ' ' 单引号 ， linux中单引号会当成字符串
+
+  $c->render(text => '1');
 };
 
 app->start;
@@ -138,6 +243,7 @@ __DATA__
 % my $msg = $c->flash('msg');
 % my $single_date_flag = $c->flash('single_date_flag');
 % my $main_info = $c->flash('main_info');
+% my $today_relative = $c->flash('today_relative');
 
 <div style="position: relative; width: 100%; height: 300px; border: 1px solid gray; border-radius: 5px;">
   <form action="/api/calc_date" method="post">
@@ -170,6 +276,26 @@ __DATA__
     <p style="text-align: center;">
     <%= $fir_date %> 0时UTC秒数为<%= $main_info->{'fir_UTC_secs'} %>
     </p>
+    <hr>
+      <p style="text-align: center;">
+      <%= $fir_date_info_hash->{fir_y} . '-' . $fir_date_info_hash->{fir_m} %>
+      </p>
+      % my $day_idx = 0;
+      % foreach my $day(@{$today_relative->{month_days}}) {
+        % $day_idx += 1;
+        % my $background_color = 'white';
+        % $background_color = 'LightSkyBlue' if $day->[0] == $fir_date_info_hash->{fir_d};
+        <span style="margin-left: 3.3rem; background-color: <%= $background_color %>" onclick="set_note('<%= $fir_date_info_hash->{fir_y} %>', '<%= $fir_date_info_hash->{fir_m} %>', this.innerText)">
+          % if ($day->[2]) {
+              <b><%= $day->[0] %></b>
+          % } else {
+              <%= $day->[0] %>
+          % }
+        </span>
+        % if ($day_idx % 7 == 0) {
+          <hr style="width: 80%; height: 1rem;">
+        % }
+      % }
   % }
   % if (defined $single_date_flag and !$single_date_flag) {
     <p style="text-align: center;">
@@ -201,8 +327,42 @@ __DATA__
   </div>
 % }
 
+<script src="./jquery-3.6.3.min.js"></script>
 <script src="./lunarFun.js"></script>
 <script type="text/javascript">
+
+  $(document).ready(function() {
+    if (document.getElementById('sec_date').value == '') {
+      $('#submit').click(); // when document loaded, auto submit once.
+    }
+  });
+
+  function set_note(y, m, day) {
+    // query
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", '/query/'+y+'/'+m+'/'+day, false);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        //let data = xhr.responseText;
+        eval("var data="+xhr.responseText);
+        let note=prompt(data.dt, data.content);
+        if (note!=null && note!="")
+        {
+          // send set note
+          const xhr2 = new XMLHttpRequest();
+          xhr2.open("POST", '/add/'+y+'/'+m+'/'+day, false);
+          xhr2.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+          
+          xhr2.send(note);
+        }
+      }
+    }
+    xhr.send();
+
+  }
+
+
   let fir_y = '<%= $fir_date_info_hash->{fir_y} %>';
   let fir_m = '<%= $fir_date_info_hash->{fir_m} %>';
   let fir_d = '<%= $fir_date_info_hash->{fir_d} %>';
