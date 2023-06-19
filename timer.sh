ac4de002bde6d1a0e0b9d926f362d76243537979
luckywyy <862024320@qq.com>
2023-06-19 10:50:44 +0800

    Usage added

--- timer.sh
+++ timer.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
-a=1
-while [ $a -eq 1 ]
-do
-    clear
-    # date
-    t1=`date +%s`
-    flag=`date +%H`
-    if [ $flag -gt 12 ]
-    then
-        t2=`date -d "\`date +%Y-%m-%d\` 18:00" +%s`
-    else
-        t2=`date -d "\`date +%Y-%m-%d\` 11:30" +%s`
-    fi
-    let "d1=$t2-$t1"
-    let "h1=$d1/(60*60)"
-    let "m1=$d1/60-$h1*60"
-    let "s1=$d1%60"
-    if [ $h1 -lt 10 ]
-    then
-        h1="0"$h1
-    fi
-    if [ $m1 -lt 10 ]
-    then
-        m1="0"$m1
-    fi
-    if [ $s1 -lt 10 ]
-    then
-        s1="0"$s1
-    fi
-    echo -e "\n\t$d1\t$h1:$m1:$s1"
-    sleep 1
-done
+if [ -z "$1" ]
+then
+    echo "Usage: chmod 777 ./timer.sh; ./timer.sh secs-offset"
+    echo -e "       e.g."
+    echo -e "       ./timer.sh 0"
+else
+    a=1
+    while [ $a -eq 1 ]
+    do
+        clear
+        # date
+        t1=`date +%s`
+        flag=`date +%H`
+        if [ $flag -gt 12 ]
+        then
+            t2=`date -d "\`date +%Y-%m-%d\` 18:00" +%s`
+        else
+            t2=`date -d "\`date +%Y-%m-%d\` 11:30" +%s`
+        fi
+        let "d1=$t2-$t1+$1"
+        let "h1=$d1/(60*60)"
+        let "m1=$d1/60-$h1*60"
+        let "s1=$d1%60"
+        if [ $h1 -lt 10 ]
+        then
+            h1="0"$h1
+        fi
+        if [ $m1 -lt 10 ]
+        then
+            m1="0"$m1
+        fi
+        if [ $s1 -lt 10 ]
+        then
+            s1="0"$s1
+        fi
+        # echo -e "\n\t$d1\t$h1:$m1:$s1"
+        echo -e "\n\t$d1"
+        sleep 1
+    done
+fi
